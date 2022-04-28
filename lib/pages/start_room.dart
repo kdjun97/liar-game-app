640eba4576b14a926d76dced8cada1fafeede492
kdjun97 <kdjun97@gmail.com>
2022-04-28 21:45:47 +0900

    Fix: Bottom overflow SingleChildScrollView 사용, overflow해결

--- lib/pages/start_room.dart
+++ lib/pages/start_room.dart
@@ -6,19 +6,21 @@ class StartRoom extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Container(
-              margin: const EdgeInsets.only(bottom: 15.0),
-              child: Image.asset('assets/배너.png')
-            ),
-            const CustomRaisedButton(title: "방만들기", condition: 0),
-            const CustomRaisedButton(title: "접속하기", condition: 1),
-            const CustomRaisedButton(title: "게임방법", condition: 2),
-            const CustomRaisedButton(title: "IP셋팅법", condition: 3),
-          ],
+      body: SingleChildScrollView(
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                margin: const EdgeInsets.only(bottom: 15.0),
+                child: Image.asset('assets/배너.png')
+              ),
+              const CustomRaisedButton(title: "방만들기", condition: 0),
+              const CustomRaisedButton(title: "접속하기", condition: 1),
+              const CustomRaisedButton(title: "게임방법", condition: 2),
+              const CustomRaisedButton(title: "IP셋팅법", condition: 3),
+            ],
+          ),
         ),
       ),
     );
